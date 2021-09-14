c18711d82063049f77e062a2214b08e72004eac3
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2021-09-14 13:57:29 +0900

    Specify revision of queries explicitly

--- show_tables.js
+++ show_tables.js
@@ -3,7 +3,7 @@ let currentTaxonName = null;
 
 const dbpedia_endpoint = 'https://dbpedia.org/sparql';
 const endpoint = 'https://orth.dbcls.jp/sparql-proxy';
-const sparql_dir = 'https://github.com/sparqling/taxonomy-browser/blob/main/sparql/'
+const sparql_dir = 'https://github.com/sparqling/taxonomy-browser/blob/7abfb0d/sparql/'
 
 
 function queryBySpang(queryUrl, param, callback, target_end = null) {
